68eab554c4655b495e1e00f4a68edc608d3a5b51
Charlie <achanont@gmail.com>
2022-08-10 19:18:51 +0700

    fixed meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home = ({
   return (
     <div className={styles.container}>
       <Head>
-        <title>TV Bland</title>
+        <title>Home Page: TV Bland</title>
         <meta name="description" content="TV Bland Application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
