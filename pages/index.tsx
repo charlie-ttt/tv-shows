8920aca2dd500008870a94678475f00a93300396
Charlie <achanont@gmail.com>
2022-08-12 21:33:13 +0700

    handle api error through UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
 import type { AxiosResponse } from "axios";
+import Error from "next/error";
 import Head from "next/head";
-import { InferGetServerSidePropsType } from "next";
 import LatestShows from "../src/components/latest-shows";
 import type { RecentShowDetail } from "../src/interfaces/api-interfaces";
 import axios from "axios";
@@ -8,7 +10,10 @@ import styles from "../styles/home-page.module.css";
 
 const Home = ({
   data,
+  errorCode,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (errorCode) return <Error statusCode={errorCode} />;
+
   return (
     <div>
       <Head>
@@ -25,19 +30,38 @@ const Home = ({
           character information.
         </p>
         <h2 className={styles.latestshows}>Latest Added Shows</h2>
-        <LatestShows movies={data} />
+        {data && <LatestShows movies={data} />}
       </main>
       <footer className={styles.footer}></footer>
     </div>
   );
 };
 
-export async function getServerSideProps() {
-  const API_ENDPOINT = process.env.TVMAZE_ENDPOINT;
-  const res: AxiosResponse<RecentShowDetail[]> = await axios.get(
-    `${API_ENDPOINT}/schedule`
-  );
-  return { props: { data: res.data } };
+interface Props {
+  data: RecentShowDetail[] | null;
+  errorCode: number | null;
 }
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const API_ENDPOINT = process.env.TVMAZE_ENDPOINT;
+  try {
+    const res: AxiosResponse<RecentShowDetail[]> = await axios.get(
+      `${API_ENDPOINT}/schedule`
+    );
+    return {
+      props: {
+        data: res.data,
+        errorCode: null,
+      },
+    };
+  } catch (error: any) {
+    return {
+      props: {
+        data: null,
+        errorCode: 500,
+      },
+    };
+  }
+};
 
 export default Home;
